79bae7ec581f0984fe052f9db58b6489104d35bc
Sebastian Juárez <84484212+SebasJuarez@users.noreply.github.com>
2023-04-11 18:47:33 -0600

    update 2

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,7 @@
           use: {
             loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env',"@babel/preset-react"]
             }
             
           }
